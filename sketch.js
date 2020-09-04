7ae4d8672c937b595cf8d90b0643ed80a299d581
4austinpowers <4austinpowers@gmail.com>
2020-09-05 02:18:44 +0400

    full polynomial with degree, optimizer and learning rate

--- sketch.js
+++ sketch.js
@@ -4,14 +4,15 @@ let x_vals = []; // array of values - not a tensor
 let y_vals = []; // array of values - not a tensor
 let a = []; // ax^n tf trainable variables
 let polyDegree; // degree of poly
-
+let optimizer; // optimizer
+let learningRate; // learning rate
 
 function setup() {
   createCanvas(400, 400);
   background(0);
 
 
-  // add slider
+  // add slider for degree of polynom
   text1 = createP('degree of polynom');
   text1.position(width / 2 - 60, height);
 
@@ -21,9 +22,44 @@ function setup() {
 
   text2 = createP(slider.value());
   text2.position(width / 2, height + 50);
-  // add button
+
+  // add select for optimizer selection
+  selector = createSelect();
+  selector.position(width / 2, height + 90);
+  selector.option('sgd');
+  selector.option('adam');
+  selector.option('adagrad');
+  selector.option('adadelta');
+  selector.option('adamax');
+  //selector.option('rmsprop');
+
+  selector.selected('sgd');
+  selector.changed(changeOptimizer);
+
+  text3 = createP('select optimizer');
+  text3.position(width / 3 - 50, height + 75);
+
+  // add slider for learning rate
+  selector1 = createSelect();
+  selector1.position(width / 2, height + 115);
+  selector1.option(2);
+  selector1.option(0.8);
+  selector1.option(0.5);
+  selector1.option(0.3);
+  selector1.option(0.1);
+  selector1.option(0.03);
+  selector1.option(0.01);
+  selector1.option(0.003);
+  selector1.option(0.001);
+  selector1.selected(0.03);
+  selector1.changed(changeLR);
+
+  text4 = createP('learning rate');
+  text4.position(width / 3 - 50, height + 100);
 
   polynomCreation();
+  changeLR();
+  changeOptimizer();
 
 }
 
@@ -46,10 +82,14 @@ function polynomCreation() {
 
 //* CREATE DATASET
 function mouseClicked() {
-  let x = map(mouseX, 0, width, -1, 1);
-  let y = map(mouseY, 0, height, 1, -1);
-  x_vals.push(x);
-  y_vals.push(y);
+  // if it's on canvas
+  if ((mouseX <= width) && (mouseY <= height)) {
+    // create the data
+    let x = map(mouseX, 0, width, -1, 1);
+    let y = map(mouseY, 0, height, 1, -1);
+    x_vals.push(x);
+    y_vals.push(y);
+  }
 }
 
 //* LOSS
@@ -60,12 +100,35 @@ function loss(predictions, labels) {
 }
 
 //* OPTIMIZER
-const learningRate = 0.03;
+
+function changeLR() {
+  learningRate = selector1.value()
+}
+//const learningRate = 0.03;
+
+
 // stochastic gradient descent
 //const optimizer = tf.train.sgd(learningRate); 
 // adam
-const optimizer = tf.train.adam(learningRate);
+//optimizer = tf.train.adam(learningRate);
 
+
+function changeOptimizer() {
+  if (selector.value() == 'sgd') {
+    optimizer = tf.train.sgd(learningRate);
+  } else if (selector.value() == 'adam') {
+    optimizer = tf.train.adam(learningRate);
+  } else if (selector.value() == 'adagrad') {
+    optimizer = tf.train.adagrad(learningRate);
+  } else if (selector.value() == 'adadelta') {
+    optimizer = tf.train.adadelta(learningRate);
+  } else if (selector.value() == 'adamax') {
+    optimizer = tf.train.adamax(learningRate);
+  } //else if (selector.value() == 'rmsprop') {
+  //   optimizer = tf.train.rmsprop(learningRate);
+  // }
+
+}
 
 //* PREDICT
 function predict(x) {
